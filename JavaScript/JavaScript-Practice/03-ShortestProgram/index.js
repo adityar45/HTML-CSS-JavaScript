63b258ab33c393e7647829a03a2ba19fa9bbadf6
Aditya Rasal <adityarasal005@gmail.com>
2024-06-20 00:46:15 +0530

    function-in-js

---
+++ JavaScript/JavaScript-Practice/03-ShortestProgram/index.js
@@ -0,0 +1,14 @@
+//Whenever a js program is created in global executin is created and window object
+//is created by js engine
+
+//window -> will give window object
+//this -> will also point towards window object
+
+// var a = 10;
+//function b() {
+//  var a = 20;
+//}
+
+//this.a -> will point towards global a i.e 10
+
+
