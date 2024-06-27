bcd1411e409c8d3d219bfa304dc7600d215e07c3
Aditya Rasal <adityarasal005@gmail.com>
2024-06-27 11:09:44 +0530

    1

--- JavaScript/JavaScript-Practice/12-CallbackFunctions/index.js
+++ JavaScript/JavaScript-Practice/12-CallbackFunctions/index.js
@@ -1,4 +1,20 @@
-//What is callback func in js
+//What is callback func in js ->
+//A function passed to another function
+//gives the power of asynchronous does not wait for the setTimeout to finish
+
+setTimeout(function () {
+    console.log("timer");
+}, 5000);
+
+function x(y) {
+    console.log("x");
+    y();
+}
+
+//y is a callback function
+x(function y() {
+    console.log("y");
+})
 
 //Javascript is a synchronous and single threaded language
 
@@ -11,5 +27,13 @@
 //Scope Demo With Event Listeners
 
 //Garbage collection & removeEventListeners
+
+function attachEventListeners() {
+document.getElementById("call").addEventListener("click", function xyz() {
+    console.log("Button clicked !");
+});
+}
+
+attachEventListeners();
 
 
